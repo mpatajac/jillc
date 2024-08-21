da47009e92193fef46f7138107e9dbcb5c62357d
mpatajac <patajacmatija3@gmail.com>
2024-08-21 20:37:09 +0200

    implement source file loading

--- src/fileio.rs
+++ src/fileio.rs
@@ -31,6 +31,125 @@ fn check_root_path(root_path: &Path) -> io::Result<()> {
     }
 }
 
+pub mod input {
+    use std::{
+        fs, io,
+        path::{Path, PathBuf},
+    };
+
+    pub struct SourceFile {
+        src_path: PathBuf,
+        file_path: PathBuf,
+        content: String,
+    }
+
+    impl SourceFile {
+        pub fn content(&self) -> &str {
+            &self.content
+        }
+
+        /// Convert file's path to a Hack-compatible module name
+        pub fn module_name(&self) -> String {
+            // get the path from the ./src dir to the source file
+            let module_path = self
+                .file_path
+                .strip_prefix(&self.src_path)
+                .expect("source file path should start with the ./src path");
+
+            module_path
+                // remove extension, so we only have the dir/file names
+                .with_extension("")
+                // split into a series of directory/file names
+                .components()
+                // convert to ordinary strings
+                // TODO: re-check string handling
+                .map(|c| c.as_os_str().to_string_lossy().to_string())
+                // concat names using "_" as a separator
+                .collect::<Vec<_>>()
+                .join("_")
+        }
+    }
+
+    pub struct SourceDir {
+        src_path: PathBuf,
+        current_file_index: usize,
+        source_file_paths: Vec<PathBuf>,
+    }
+
+    fn check_src_exists(root_path: &Path) -> io::Result<PathBuf> {
+        let src_path = root_path.join("src");
+        if src_path.is_dir() {
+            Ok(src_path)
+        } else {
+            Err(io::Error::other(
+                "provided root path does not contain a `src` directory",
+            ))
+        }
+    }
+
+    impl SourceDir {
+        pub fn setup(root_path: &Path) -> io::Result<Self> {
+            super::check_root_path(root_path)?;
+
+            let src_path = check_src_exists(root_path)?;
+
+            let mut source_file_paths = vec![];
+            Self::collect_source_files(&src_path, &mut source_file_paths)?;
+
+            Ok(Self {
+                src_path,
+                current_file_index: 0,
+                source_file_paths,
+            })
+        }
+
+        fn collect_source_files(dir: &PathBuf, collected: &mut Vec<PathBuf>) -> io::Result<()> {
+            for entry in fs::read_dir(dir)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.is_dir() {
+                    Self::collect_source_files(&path, collected)?;
+                } else {
+                    collected.push(path);
+                }
+            }
+
+            Ok(())
+        }
+    }
+
+    impl Iterator for SourceDir {
+        type Item = io::Result<SourceFile>;
+
+        // TODO?: iterate only over file names and have a separate function for loading?
+        fn next(&mut self) -> Option<Self::Item> {
+            // reached the end of the file list - no more files to load
+            if self.current_file_index == self.source_file_paths.len() {
+                return None;
+            }
+
+            // get the path to the current (first unread) file
+            let file_path = self.source_file_paths[self.current_file_index].clone();
+
+            // move index to next file (for next iteration)
+            self.current_file_index += 1;
+
+            // (try to) read the contents of the file
+            let read_result = fs::read_to_string(&file_path);
+
+            // on successful read, store content (and other metadata) to model
+            let source_file_result = read_result.map(|content| SourceFile {
+                src_path: self.src_path.clone(),
+                file_path,
+                content,
+            });
+
+            // NOTE: keep as result (as any of the file reads could fail), handle on per-file basis
+            Some(source_file_result)
+        }
+    }
+}
+
 pub mod output {
     use std::{
         fs,
