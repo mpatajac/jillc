84b9959ca29999200339cd02802790f22631b942
mpatajac <patajacmatija3@gmail.com>
2024-08-22 20:14:11 +0200

    make expression definition recursive

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -38,39 +38,41 @@ fn parser() -> impl Parser<char, JillModuleContent, Error = JillParseError> {
         .map(|chars| chars.into_iter().collect())
         .map(JillLiteral::String);
 
-    let literal = integer.or(string).map(JillExpression::Literal);
-
     let identifier = text::ident().padded().map(JillIdentifier);
 
-    let variable_name = identifier.map(JillExpression::VariableName);
+    let expression = recursive(|expression| {
+        let literal = integer.or(string).map(JillExpression::Literal);
 
-    // ( :: | ( mod_name:: )+ )
-    let local_module_path = just("::").to(Vec::new());
-    let modules_path = identifier
-        .then_ignore(just("::"))
-        .repeated()
-        .at_least(1)
-        .or(local_module_path);
+        let variable_name = identifier.map(JillExpression::VariableName);
 
-    let associated_type = identifier.then_ignore(just(":"));
-    let function_name = identifier;
-    let function_reference = modules_path
-        .then(associated_type.or_not())
-        .then(function_name)
-        .map(
-            |((modules_path, associated_type), function_name)| JillFunctionReference {
-                modules_path,
-                associated_type,
-                function_name,
-            },
-        );
+        // ( :: | ( mod_name:: )+ )
+        let local_module_path = just("::").to(Vec::new());
+        let modules_path = identifier
+            .then_ignore(just("::"))
+            .repeated()
+            .at_least(1)
+            .or(local_module_path);
 
-    // since there is possible ambiguity here, order in which the
-    // options are listed is important (most specific => least specific)
-    let expression = literal
-        .or(function_reference.map(JillExpression::FunctionReference))
-        .or(variable_name)
-        .padded();
+        let associated_type = identifier.then_ignore(just(":"));
+        let function_name = identifier;
+        let function_reference = modules_path
+            .then(associated_type.or_not())
+            .then(function_name)
+            .map(
+                |((modules_path, associated_type), function_name)| JillFunctionReference {
+                    modules_path,
+                    associated_type,
+                    function_name,
+                },
+            );
+
+        // since there is possible ambiguity here, order in which the
+        // options are listed is important (most specific => least specific)
+        literal
+            .or(function_reference.map(JillExpression::FunctionReference))
+            .or(variable_name)
+            .padded()
+    });
 
     let variable = text::keyword("let")
         .ignore_then(identifier)
